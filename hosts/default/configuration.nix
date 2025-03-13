d1c8c60b7b7d64e691e335f1ee8be9d5bdc596b8
Klimt <nixos@theklim.com>
2025-03-13 13:29:19 +0100

    Fix submodule issue and re-add hosts/default

---
+++ hosts/default/configuration.nix
@@ -0,0 +1,226 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+#imports = [ <home-manager/nixos> ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1"];
+    services.dnscrypt-proxy2 = {
+    enable = true;
+    # Settings reference:
+    # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+      # Add this to test if dnscrypt-proxy is actually used to resolve DNS requests
+      query_log.file = "/var/log/dnscrypt-proxy/query.log";
+      sources.public-resolvers = {
+        urls = [
+        "https://doh.applied-privacy.net/query"
+        ];
+        cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      # server_names = [ ... ];
+    };
+  };
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.blueman.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Vienna";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nixi = {
+    isNormalUser = true;
+    description = "nixi";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+  };
+
+  home-manager = {
+     extraSpecialArgs = { inherit inputs;};
+     users = {
+      "nixi" = import ./home.nix;
+      };
+    };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "nixi";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    #neovim
+    git
+    firefox
+    kitty
+    waybar
+    mako
+    grim
+    slurp
+    wl-clipboard
+
+    xdg-desktop-portal
+    xdg-desktop-portal-wlr
+
+  ];
+
+    services.dbus.enable = true;
+
+
+    # Enable the gnome-keyring secrets vault. 
+    # Will be exposed through DBus to programs willing to store secrets.
+#   services.gnome.gnome-keyring.enable = true;
+
+   environment.variables = {
+    # Force Wayland for Electron Apps (Obsidian, Anydesk, Discord, VSCode, etc.)
+    NIXOS_OZONE_WL = "1";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+
+    # Wayland for Qt Applications
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+
+    # Wayland for GTK Applications
+#    GDK_BACKEND = "wayland";
+
+    # Enable Wayland portals (for file pickers, copy-paste, etc.)
+    GTK_USE_PORTAL = "1";
+  };
+
+  # Enable XWayland (for compatibility)
+  services.xserver.enable = false;
+  #services.xdg-desktop-portal.enable = true;
+
+
+  # yprland; ly is display manager
+  services.displayManager.ly.enable = true;
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+#  programs.hyprland = {
+#  enable = true;
+#  xwayland.enable = true;
+#  package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+#  };
+
+#  nix.settings = {
+#    substituters = ["https://hyprland.cachix.org"];
+#    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+#  };
+
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  # nixos helper
+    programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/etc/nixos/";
+  };
+
+  xdg.portal = {
+  enable = true;
+  extraPortals = with pkgs; [
+    xdg-desktop-portal-wlr
+    xdg-desktop-portal-gtk
+  ];
+#  wlr = {
+#    enable = true;
+#    settings = { # uninteresting for this problem, for completeness only
+#      screencast = {
+#        output_name = "eDP-1";
+#        max_fps = 30;
+#        chooser_type = "simple";
+#        chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+#      };
+#    };
+#  };
+};
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
---
+++ hosts/default/home.nix
@@ -0,0 +1,191 @@
+{ config, pkgs,inputs,lib, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "nixi";
+  home.homeDirectory = "/home/nixi";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+    kickoff
+#    cura-appimage
+    bemenu # need for screenshots
+    htop
+    btop
+    swayimg
+    acpi # battery show
+    swayrbar # bar for sway
+    rofi-wayland
+    brightnessctl
+    anydesk
+    obsidian
+    wofi
+    font-awesome
+    alejandra
+#    zsh-autosuggestions
+    zsh
+    oh-my-zsh
+    telegram-desktop
+#    nwg-displays #as arandr but wland // doesn't work
+    wdisplays # also display settings
+    kanshi # too
+    pavucontrol
+    vimPlugins.vim-plug
+    fzf
+    qrencode
+    mpv
+    dnscrypt-proxy
+    
+
+
+#    wezterm # terminal emulation
+    wl-clipboard
+    tldr 
+    bat
+    hyprlock
+
+    qutebrowser
+    nfs-utils
+
+    #some stuff for hy3
+    cmake
+    meson
+    cpio
+    pkg-config
+    #gnumake
+    #cpio
+    #hyprwayland-scanner
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    nerd-fonts.jetbrains-mono
+    helvetica-neue-lt-std
+    arkpandora_ttf
+    gnome-themes-extra
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/nixi/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+    programs.wezterm = {
+      enable = true;
+      # other config...
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = [pkgs.vimPlugins.nvim-tree-lua 
+               pkgs.vimPlugins.vim-airline
+               pkgs.vimPlugins.vim-airline-themes
+               pkgs.vimPlugins.coc-yaml
+               pkgs.vimPlugins.vim-smoothie
+               pkgs.vimPlugins.gruvbox
+               pkgs.vimPlugins.yats-vim
+               pkgs.vimPlugins.vim-numbertoggle
+               pkgs.vimPlugins.vim-auto-save
+               pkgs.vimPlugins.vim-go
+               pkgs.vimPlugins.csv-vim
+               pkgs.vimPlugins.vim-tmux-navigator];
+
+  };
+
+
+
+  programs.fzf.enableZshIntegration = true;
+
+  programs.zsh = {
+  enable = true;
+  enableCompletion = true;
+  #autosuggestions.enable = true;
+  autosuggestion.enable = true;
+
+
+
+  syntaxHighlighting.enable = true;
+      initExtra = ''
+      source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+      source <(fzf --zsh)
+      ZVM_VI_INSERT_ESCAPE_BINDKEY=jj
+    '';
+
+
+  shellAliases = {
+    ll = "ls -l";
+    vi = "nvim";
+    update = "nh os switch /etc/nixos";
+    cat = "bat";
+  };
+  history.size = 10000;
+
+  oh-my-zsh = {
+     enable = true;
+     plugins = [];
+     theme = "agnoster";
+     };
+
+};
+
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
