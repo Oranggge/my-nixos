5c23bc704597ec5f640b193c79e0f6c62c0296a5
Klimt <nixos@theklim.com>
2025-03-26 14:01:24 +0100

    added i3 to windowsmanager

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -158,7 +158,7 @@
 #  };
 
   # Enable XWayland (for compatibility)
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
   #services.xdg-desktop-portal.enable = true;
 
 
@@ -169,6 +169,31 @@
     enable = true;
     wrapperFeatures.gtk = true;
   };
+
+
+  environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
+   services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+   
+    displayManager = {
+        defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+     ];
+    };
+  };
+
 
 #  programs.hyprland = {
 #  enable = true;
