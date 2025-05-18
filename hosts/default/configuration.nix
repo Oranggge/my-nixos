5bb7a9bc7466233060248f90117d55b526f9c30a
Klimt <nixos@theklim.com>
2025-05-18 20:45:01 +0200

    autoupgrade enabled with flake, hopefully

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -11,7 +11,17 @@
     ];
 
 #imports = [ <home-manager/nixos> ];
-  system.autoUpgrade.enable  = true;
+system.autoUpgrade = {
+  enable = true;
+  flake = inputs.self.outPath;
+  flags = [
+    "--update-input"
+    "nixpkgs"
+    "-L" # print build logs
+  ];
+  dates = "02:00";
+  randomizedDelaySec = "45min";
+};
 
 
   # Bootloader.
